ce100a40bb356e887b40cb51ec762efa0466a1da
sidkr10 <sinhasiddharth1998@gmail.com>
2024-12-22 18:50:05 +0000

    Parsing correlation ID

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,14 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <vector>
+
+struct ClientMessage {
+    u_int32_t message_size;
+    u_int16_t request_api_key;
+    u_int16_t request_api_version;
+    u_int32_t correlation_id;
+};
 
 int main(int argc, char* argv[]) {
     // Disable output buffering
@@ -59,14 +67,37 @@ int main(int argc, char* argv[]) {
     int client_fd = accept(server_fd, reinterpret_cast<struct sockaddr*>(&client_addr), &client_addr_len);
     std::cout << "Client connected\n";
     
-    int m_size = htonl(5);
-    int cid = htonl(7);
+    // read from client
     
-    write(client_fd, &m_size, 4);
-    write(client_fd, &cid, 4);
+    int n;
+    char buffer[255];
+    n = read(client_fd, &buffer, sizeof(buffer));
 
-    std::cout << "Message sent!\n";
-    
+    if(n > 0) {
+        int msg_size = 4;
+        int cid;
+        ClientMessage clientMsg;
+        size_t offset = 0;
+        clientMsg.message_size = htonl((buffer[offset] << 24) | (buffer[offset + 1] << 16) |
+                         (buffer[offset + 2] << 8) | buffer[offset + 3]);
+        offset += 4;
+        clientMsg.request_api_key = htonl((buffer[offset] << 8) | buffer[offset +1]);
+        offset += 2;
+        clientMsg.request_api_version = htonl((buffer[offset] << 8) | buffer[offset +1]);
+        offset += 2;
+        clientMsg.correlation_id = htonl((buffer[offset] << 24) | (buffer[offset + 1] << 16) |
+                         (buffer[offset + 2] << 8) | buffer[offset + 3]);
+        offset += 4;
+        
+        // write response message to client
+
+        write(client_fd, &clientMsg.message_size, sizeof(clientMsg.message_size));
+        write(client_fd, &clientMsg.correlation_id, sizeof(clientMsg.correlation_id));
+
+    } else {
+        std::cout << "No message received from client\n"; 
+    }
+
     close(client_fd);
 
     close(server_fd);
